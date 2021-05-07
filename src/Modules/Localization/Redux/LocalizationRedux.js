76e34c69d14d52ce02f2b0554ee11aee50046e07
leylasonmez <59195532+leylasonmez@users.noreply.github.com>
2021-05-07 12:43:59 +0300

    startproject

---
+++ src/Modules/Localization/Redux/LocalizationRedux.js
@@ -0,0 +1,38 @@
+
+/* ------------- Initial State ------------- */
+
+export const INITIAL_STATE = {
+    locale: 'tr',
+};
+
+/* ------------- Types and Action Creators ------------- */
+
+const Types = {
+    CHANGE_LOCALE: 'locale/change_locale',
+};
+
+const Actions = {
+    changeLocale: ({ locale }) => ({
+        type: Types.CHANGE_LOCALE,
+        payload: { locale },
+    }),
+};
+
+export const LocalizationTypes = Types;
+export const LocalizationActions = Actions;
+
+/* ------------- Selectors ------------- */
+
+export const LocalizationSelectors = {
+    locale: state => state.localization.locale,
+  };
+
+/* ------------- Reducers ------------- */
+
+export const changeLocale = (state, action) => {
+    let { locale } = action.payload;
+    return {
+        ...state,
+        locale,
+    };
+};
---
+++ src/Modules/Theming/index.js
@@ -0,0 +1,10 @@
+export { 
+    useTheme,
+    useThemedColors, 
+    useThemedStyles, 
+    useThemedOption, 
+    useDispatchChangeTheme,
+ } from './CustomHooks/ThemingHooks';
+export { ThemeReducer } from './Redux/ThemingRedux';
+export { colorNames } from './Color';
+export { ThemeModes } from './ThemingConstants';
