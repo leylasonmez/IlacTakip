866d302727f704438b21208d0dcafe8879094e70
leylasonmez <59195532+leylasonmez@users.noreply.github.com>
2021-05-07 15:18:53 +0300

    navigation base

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import MainNavigation from './Navigation/MainNavigation';
 
 const App = () => {
   return (
-    <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-      <Text>Pill Tracker</Text>
-    </View>
+    <MainNavigation/>
   )
 }
 
